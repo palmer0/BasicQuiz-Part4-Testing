693159657253ad82c829c8bb08642024a4ddd00c
Luis Hernandez <lhdez@iuma.ulpgc.es>
2023-10-05 17:15:02 +0100

    finalizada implementacion de tests para app Quiz

--- app/src/androidTest/java/es/ulpgc/eite/da/basicquiz/QuizSteps.java
+++ app/src/androidTest/java/es/ulpgc/eite/da/basicquiz/QuizSteps.java
@@ -4,76 +4,23 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.Matchers.not;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.RemoteException;
 
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.rule.ActivityTestRule;
 import androidx.test.uiautomator.UiDevice;
 
-import org.junit.Rule;
-
-import es.ulpgc.eite.da.basicquiz.QuestionActivity;
-import es.ulpgc.eite.da.basicquiz.R;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
-@SuppressWarnings("ALL")
 public class QuizSteps {
+
 
   private static final int DELAY_IN_SECS = 0 * 1000;
 
-  @Rule
-  public ActivityTestRule<QuestionActivity> testRule =
-      new ActivityTestRule(QuestionActivity.class, true, false);
-
-  private Activity activity;
-
-
-  @Before("@quiz-feature")
-  public void setUp() {
-
-    try {
-
-      UiDevice device = UiDevice.getInstance(getInstrumentation());
-      device.setOrientationNatural();
-
-    } catch (RemoteException e) {
-    }
-
-    testRule.launchActivity(new Intent());
-    activity = testRule.getActivity();
-  }
-
-  @After("@quiz-feature")
-  public void tearDown() {
-
-    try {
-
-      UiDevice device = UiDevice.getInstance(getInstrumentation());
-      device.setOrientationNatural();
-
-    } catch (RemoteException e) {
-    }
-
-    testRule.finishActivity();
-  }
-
-
-  @Given("^iniciar pantalla Question$")
   public void iniciarPantallaQuestion() {
 
     /*
@@ -91,49 +38,53 @@ public class QuizSteps {
 
   }
 
-  @And("^mostrar pregunta \"([^\"]*)\"$")
   public void mostrarPregunta(String q) {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.questionText)).check(matches(isDisplayed()));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.questionText)).check(matches(withText(q)));
+    onView(withId(R.id.questionText)).check(matches(isCompletelyDisplayed()));
+    onView(withId(R.id.questionText)).check(matches(withText(q)));
   }
 
-  @And("^ocultar resultado$")
-  public void ocultarResultado() {
-    //onView(withId(R.id.questionText)).check(matches(not(isDisplayed())));
-    //onView(withId(R.id.replyText)).check(matches(isDisplayed()));
+  public void ocultarResultado(String r) {
+    //onView(withId(R.id.questionText)).check(matches(not(isCompletelyDisplayed())));
+    //onView(withId(R.id.replyText)).check(matches(isCompletelyDisplayed()));
     //onView(withId(R.id.replyText)).check(matches(withText("???")));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.replyText))
-        .check(matches(withText(activity.getString(es.ulpgc.eite.da.basicquiz.R.string.empty_text))));
+    onView(withId(R.id.replyText)).check(matches(withText(r)));
   }
 
 
-  @And("^ocultar respuesta$")
-  public void ocultarRespuesta() {
-    //onView(withId(R.id.answerText)).check(matches(isDisplayed()));
+  public void ocultarRespuesta(String r) {
+    //onView(withId(R.id.answerText)).check(matches(isCompletelyDisplayed()));
     //onView(withId(R.id.answerText)).check(matches(withText("???")));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.answerText))
-        .check(matches(withText(activity.getString(es.ulpgc.eite.da.basicquiz.R.string.empty_text))));
+    onView(withId(R.id.answerText)).check(matches(withText(r)));
   }
 
-  @And("^mostrar botones True y False y Cheat activados$")
   public void mostrarBotonesTrueYFalseYCheatActivados() {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.trueButton)).check(matches(isEnabled()));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.falseButton)).check(matches(isEnabled()));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.cheatButton)).check(matches(isEnabled()));
+    onView(withId(R.id.trueButton)).check(matches(isEnabled()));
+    onView(withId(R.id.falseButton)).check(matches(isEnabled()));
+    onView(withId(R.id.cheatButton)).check(matches(isEnabled()));
   }
 
-  @And("^mostrar boton Next desactivado$")
   public void mostrarBotonNextDesactivado() {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.nextButton)).check(matches(not(isEnabled())));
+    onView(withId(R.id.nextButton)).check(matches(not(isEnabled())));
   }
 
-  @When("^pulsar boton \"([^\"]*)\"$")
-  public void pulsarBoton(String b) {
+//  public void pulsarBoton(String b, String tb) {
+//
+//    //String tb = activity.getString(R.string.true_button_text);
+//    //int button = (b.equals("True")) ? R.id.trueButton : R.id.falseButton;
+//    int button = (b.equals(tb)) ? R.id.trueButton : R.id.falseButton;
+//    onView(withId(button)).check(matches(isCompletelyDisplayed()));
+//    onView(withId(button)).perform(click());
+//
+//    try {
+//      Thread.sleep(DELAY_IN_SECS);
+//    } catch (InterruptedException e) {
+//    }
+//  }
 
-    String tb = activity.getString(es.ulpgc.eite.da.basicquiz.R.string.true_button_text);
-    //int button = (b.equals("True")) ? R.id.trueButton : R.id.falseButton;
-    int button = (b.equals(tb)) ? es.ulpgc.eite.da.basicquiz.R.id.trueButton : es.ulpgc.eite.da.basicquiz.R.id.falseButton;
-    onView(withId(button)).check(matches(isDisplayed()));
+
+  public void pulsarBoton(int button) {
+
+    onView(withId(button)).check(matches(isCompletelyDisplayed()));
     onView(withId(button)).perform(click());
 
     try {
@@ -142,30 +93,33 @@ public class QuizSteps {
     }
   }
 
-  @Then("^mostrar resultado \"([^\"]*)\" a respuesta \"([^\"]*)\"$")
-  public void mostrarResultadoARespuesta(String r, String a) {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.replyText)).check(matches(isDisplayed()));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.replyText)).check(matches(withText(r)));
+//  public void mostrarResultadoARespuesta(String r, String a) {
+//    onView(withId(R.id.replyText)).check(matches(isCompletelyDisplayed()));
+//    onView(withId(R.id.replyText)).check(matches(withText(a)));
+//  }
+
+
+  public void mostrarResultadoARespuesta(String reply) {
+    onView(withId(R.id.replyText)).check(matches(isCompletelyDisplayed()));
+    onView(withId(R.id.replyText)).check(matches(withText(reply)));
   }
 
-  @And("^mostrar botones True y False y Cheat desactivados$")
+
   public void mostrarBotonesTrueYFalseYCheatDesactivados() {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.trueButton)).check(matches(not(isEnabled())));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.falseButton)).check(matches(not(isEnabled())));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.cheatButton)).check(matches(not(isEnabled())));
+    onView(withId(R.id.trueButton)).check(matches(not(isEnabled())));
+    onView(withId(R.id.falseButton)).check(matches(not(isEnabled())));
+    onView(withId(R.id.cheatButton)).check(matches(not(isEnabled())));
 
   }
 
-  @And("^mostrar boton Next activado$")
   public void mostrarBotonNextActivado() {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.nextButton)).check(matches(isEnabled()));
+    onView(withId(R.id.nextButton)).check(matches(isEnabled()));
   }
 
-  @When("^pulsar boton Cheat$")
   public void pulsarBotonCheat() {
 
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.cheatButton)).check(matches(isDisplayed()));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.cheatButton)).perform(click());
+    onView(withId(R.id.cheatButton)).check(matches(isCompletelyDisplayed()));
+    onView(withId(R.id.cheatButton)).perform(click());
 
     try {
       Thread.sleep(DELAY_IN_SECS);
@@ -173,7 +127,6 @@ public class QuizSteps {
     }
   }
 
-  @Then("^iniciar pantalla Cheat$")
   public void iniciarPantallaCheat() {
     //getInstrumentation().waitForIdleSync();
 
@@ -183,26 +136,22 @@ public class QuizSteps {
     }
   }
 
-  @And("^mostrar mensaje Warning$")
-  public void mostrarMensajeWarning() {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.warningText)).check(matches(isDisplayed()));
+  public void mostrarMensajeWarning(String w) {
+    onView(withId(R.id.warningText)).check(matches(isCompletelyDisplayed()));
     //onView(withId(R.id.warningText))
     //    .check(matches(withText("Are you sure?")));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.warningText))
-        .check(matches(withText(activity.getString(es.ulpgc.eite.da.basicquiz.R.string.warning_text))));
+    onView(withId(R.id.warningText)).check(matches(withText(w)));
   }
 
-  @And("^mostrar botones Yes y No activados$")
   public void mostrarBotonesYesYNoActivados() {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.yesButton)).check(matches(isEnabled()));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.noButton)).check(matches(isEnabled()));
+    onView(withId(R.id.yesButton)).check(matches(isEnabled()));
+    onView(withId(R.id.noButton)).check(matches(isEnabled()));
   }
 
-  @When("^pulsar boton No$")
   public void pulsarBotonNo() {
 
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.noButton)).check(matches(isDisplayed()));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.noButton)).perform(click());
+    onView(withId(R.id.noButton)).check(matches(isCompletelyDisplayed()));
+    onView(withId(R.id.noButton)).perform(click());
 
     try {
       Thread.sleep(DELAY_IN_SECS);
@@ -210,7 +159,6 @@ public class QuizSteps {
     }
   }
 
-  @Then("^finalizar pantalla Cheat$")
   public void finalizarPantallaCheat() {
     try {
       Thread.sleep(DELAY_IN_SECS);
@@ -218,7 +166,6 @@ public class QuizSteps {
     }
   }
 
-  @And("^resumir pantalla Question$")
   public void resumirPantallaQuestion() {
     try {
       Thread.sleep(DELAY_IN_SECS);
@@ -226,11 +173,10 @@ public class QuizSteps {
     }
   }
 
-  @When("^pulsar boton Yes$")
   public void pulsarBotonYes() {
 
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.yesButton)).check(matches(isDisplayed()));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.yesButton)).perform(click());
+    onView(withId(R.id.yesButton)).check(matches(isCompletelyDisplayed()));
+    onView(withId(R.id.yesButton)).perform(click());
 
     try {
       Thread.sleep(DELAY_IN_SECS);
@@ -238,20 +184,24 @@ public class QuizSteps {
     }
   }
 
-  @Then("^mostrar respuesta \"([^\"]*)\" a pregunta \"([^\"]*)\"$")
-  public void mostrarRespuestaAPregunta(String a, String q) {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.answerText)).check(matches(isDisplayed()));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.answerText)).check(matches(withText(a)));
+//  public void mostrarRespuestaAPregunta(String a, String q) {
+//    onView(withId(R.id.answerText)).check(matches(isCompletelyDisplayed()));
+//    onView(withId(R.id.answerText)).check(matches(withText(a)));
+//  }
+
+
+  public void mostrarRespuestaAPregunta(String answer) {
+
+    onView(withId(R.id.answerText)).check(matches(isCompletelyDisplayed()));
+    onView(withId(R.id.answerText)).check(matches(withText(answer)));
   }
 
-  @And("^mostrar botones Yes y No desactivados$")
   public void mostrarBotonesYesYNoDesactivados() {
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.yesButton)).check(matches(not(isEnabled())));
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.noButton)).check(matches(not(isEnabled())));
+    onView(withId(R.id.yesButton)).check(matches(not(isEnabled())));
+    onView(withId(R.id.noButton)).check(matches(not(isEnabled())));
 
   }
 
-  @When("^pulsar boton Back$")
   public void pulsarBotonBack() {
     try {
       Thread.sleep(DELAY_IN_SECS);
@@ -262,11 +212,10 @@ public class QuizSteps {
     pressBack();
   }
 
-  @When("^pulsar boton Next$")
   public void pulsarBotonNext() {
 
-    onView(ViewMatchers.withId(es.ulpgc.eite.da.basicquiz.R.id.nextButton)).check(matches(isDisplayed()));
-    onView(ViewMatchers.withId(R.id.nextButton)).perform(click());
+    onView(withId(R.id.nextButton)).check(matches(isCompletelyDisplayed()));
+    onView(withId(R.id.nextButton)).perform(click());
 
     try {
       Thread.sleep(DELAY_IN_SECS);
@@ -274,10 +223,9 @@ public class QuizSteps {
     }
   }
 
-  @When("^girar pantalla$")
-  public void girarPantalla() {
+  public void girarPantalla(int orientation) {
 
-    int orientation = activity.getRequestedOrientation();
+    //int orientation = activity.getRequestedOrientation();
 
 //    if(orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
 //      orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
@@ -292,7 +240,7 @@ public class QuizSteps {
 
       UiDevice device = UiDevice.getInstance(getInstrumentation());
 
-      if(orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+      if (orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
         device.setOrientationNatural();
 
       } else {
@@ -300,7 +248,6 @@ public class QuizSteps {
       }
 
     } catch (RemoteException e) {
-
     }
 
     try {
@@ -309,5 +256,103 @@ public class QuizSteps {
     }
   }
 
+    /*
+    public void iniciarPantallaQuestion() {
 
+    }
+
+    public void mostrarPreguntaP1(String p1) {
+
+    }
+
+    public void ocultarResultado() {
+
+    }
+
+    public void mostrarBotonesTrueYFalseYCheatActivados() {
+
+    }
+
+    public void mostrarBotonNextDesactivado() {
+
+    }
+
+    public void pulsarBotonP1(String p1) {
+
+    }
+
+    public void mostrarResultadoP1ARespuestaP2(String p1, String p2) {
+
+    }
+
+    public void mostrarBotonesTrueYFalseYCheatDesactivados() {
+
+    }
+
+    public void mostrarBotonNextActivado() {
+
+    }
+
+    public void girarPantalla() {
+
+    }
+
+    public void pulsarBotonCheat() {
+
+    }
+
+    public void iniciarPantallaCheat() {
+
+    }
+
+    public void mostrarMensajeWarning() {
+
+    }
+
+    public void ocultarRespuesta() {
+
+    }
+
+    public void mostrarBotonesYesYNoActivados() {
+
+    }
+
+    public void pulsarBotonBack() {
+
+    }
+
+    public void pulsarBotonNo() {
+
+    }
+
+    public void finalizarPantallaCheat() {
+
+    }
+
+    public void resumirPantallaQuestion() {
+
+    }
+
+    public void pulsarBotonYes() {
+
+    }
+
+    public void mostrarRespuestaP1APreguntaP2(String p1, String p2) {
+
+    }
+
+    public void mostrarBotonesYesYNoDesactivados() {
+
+    }
+
+    public void resumirPantallaQuestionAndResumirPantallaQuestion() {
+
+    }
+
+    public void pulsarBotonNext() {
+
+    }
+
+
+    */
 }
